d41ecc0519e98d0caafc7221beae725236859ec1
Aswaj <aswajcheruvath@gmail.com>
2024-05-01 12:29:27 +0530

    alertboxfull

--- lib/weather/data/alerts.dart
+++ lib/weather/data/alerts.dart
@@ -1,6 +1,66 @@
-import 'dart:async';
+// import 'dart:async';
 
+// import 'package:flutter/material.dart';
+
+// class AlertBox extends StatefulWidget {
+//   @override
+//   _AlertBoxState createState() => _AlertBoxState();
+// }
+
+// class _AlertBoxState extends State<AlertBox> {
+//   int _currentIndex = 0;
+//   final List<String> _messages = [
+//     'Alert 1',
+//     'Alert 2',
+//     'Alert 3',
+//   ];
+//   late Timer _timer;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+//       setState(() {
+//         _currentIndex = (_currentIndex + 1) % _messages.length;
+//       });
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _timer.cancel();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 50,
+//       width: MediaQuery.of(context).size.width,
+//       padding: EdgeInsets.symmetric(horizontal: 16.0),
+//       decoration: BoxDecoration(
+//         color: Colors.white,
+//         borderRadius: BorderRadius.circular(8.0),
+//       ),
+//       child: AnimatedSwitcher(
+//         duration: Duration(milliseconds: 500),
+//         child: Text(
+//           _messages[_currentIndex],
+//           key: ValueKey<int>(_currentIndex),
+//           style: TextStyle(
+//             color: Colors.black,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AlertBox extends StatefulWidget {
   @override
@@ -8,20 +68,18 @@ class AlertBox extends StatefulWidget {
 }
 
 class _AlertBoxState extends State<AlertBox> {
-  int _currentIndex = 0;
-  final List<String> _messages = [
-    'Alert 1',
-    'Alert 2',
-    'Alert 3',
-  ];
+  late SharedPreferences _prefs;
   late Timer _timer;
+  int _currentIndex = 0;
+  List<String> _notifications = [];
 
   @override
   void initState() {
     super.initState();
+    _initSharedPreferences();
     _timer = Timer.periodic(Duration(seconds: 5), (timer) {
       setState(() {
-        _currentIndex = (_currentIndex + 1) % _messages.length;
+        _currentIndex = (_currentIndex + 1) % _notifications.length;
       });
     });
   }
@@ -32,6 +90,17 @@ class _AlertBoxState extends State<AlertBox> {
     super.dispose();
   }
 
+  Future<void> _initSharedPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    _loadNotifications();
+  }
+
+  Future<void> _loadNotifications() async {
+    setState(() {
+      _notifications = _prefs.getStringList('notifications') ?? [];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,20 +108,24 @@ class _AlertBoxState extends State<AlertBox> {
       width: MediaQuery.of(context).size.width,
       padding: EdgeInsets.symmetric(horizontal: 16.0),
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: const Color.fromARGB(255, 255, 255, 255),
         borderRadius: BorderRadius.circular(8.0),
       ),
       child: AnimatedSwitcher(
         duration: Duration(milliseconds: 500),
-        child: Text(
-          _messages[_currentIndex],
-          key: ValueKey<int>(_currentIndex),
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        child: _notifications.isNotEmpty
+            ? Text(
+                _notifications[_currentIndex],
+                key: ValueKey<String>(_notifications[_currentIndex]),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            : SizedBox(), // Return empty SizedBox if there are no notifications
       ),
     );
   }
 }
+
+
